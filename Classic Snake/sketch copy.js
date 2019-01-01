c356c5fdad84628aa494bf964c4eb6c332a20143
Waleed Hashmi <waleedahmedhashmi@gmail.com>
2019-01-01 21:47:59 +0500

    snake movement added

--- Classic Snake/sketch copy.js
+++ Classic Snake/sketch copy.js
@@ -1,7 +1,9 @@
 function block(x,y){
-    this.x = x;
-    this.y = y;
+    this.x = x*(canvasSize/gridSize);
+    this.y = y*(canvasSize/gridSize);
+    this.active = false; 
     this.type = 'none';
+    this.head = false;
     
     this.display = function(){
         noStroke();
@@ -13,27 +15,27 @@ function block(x,y){
         } else {
             fill (0);
         }
-        rect(this.x*(canvasSize/gridSize),this.y*(canvasSize/gridSize),canvasSize/gridSize,canvasSize/gridSize)
+        rect(this.x,this.y,canvasSize/gridSize,canvasSize/gridSize)
     }
 }
 
 function snake(){
-    this.x;             // x and y coordinates of the snake's head
-    this.y;
+    this.head = [gridSize/2,gridSize/2]; 
     this.body = []
-    this.v = 2;
-    this.dir = [[0,1],[0,-1],[1,0],[-1,0]]
+    this.v = 'right';
     
-    this.move = function(){              
+    this.body.push(this.head);
+    
+    this.moveRight = function(){
         for (var i = 0; i < this.body.length; i++){
-            if (this.body[i].x + 1<= gridSize-1){
-                board[this.body[i].x][this.body[i].y].type='none';
-                this.body[i].x += this.dir[this.v][0]; 
-                this.body[i].y += this.dir[this.v][1];
-                board[this.body[i].x][this.body[i].y].type='snake';
+            if (this.body[i][1].type=='snake' && (i+1 <= gridSize)){
+                this.body[i][1].type = 'none';
+                this.body[i-1][1].type = 'snake';
             }
-        }        
+            
+        }
     }
+
 }
 
 
@@ -44,8 +46,6 @@ var gridSize = 50;    // 10x10 pixels will make one block of snake/food
 var s = new snake; 
 
 
-
-
 function setBoard(){
     // creating an empty grid
     for (var i = 0; i< gridSize; i++){
@@ -54,14 +54,11 @@ function setBoard(){
             temp.push(new block(i,j));
         }
         board.push(temp);
-    }    
+    } 
+    
     board[gridSize/2][gridSize/2].type='snake';
-    s.body.push(board[gridSize/2][gridSize/2]);
-    s.x = gridSize/2;
-    s.y = gridSize/2;
+
 }
-
-
 
 function displayBoard(){
     for (var i = 0; i< gridSize; i++){
@@ -80,19 +77,7 @@ function setup(){
 function draw() {
     background (0);
     displayBoard();
-    s.move();
-}
-
-function keyPressed(){
-    if (keyCode === UP_ARROW){
-        s.v=0; 
-    } else if (keyCode === DOWN_ARROW){
-        s.v=1;    
-    } else if (keyCode === RIGHT_ARROW){
-        s.v=2;   
-    } else if (keyCode === LEFT_ARROW){
-        s.v=3;   
-    }             
+    s.moveRight(); 
 }
 
 
