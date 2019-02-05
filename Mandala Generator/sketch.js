3f83994289f1fbc559e1c4718301f9154de7a7f8
Waleed Hashmi <waleedahmedhashmi@gmail.com>
2019-02-05 13:40:46 +0100

    .

--- Mandala Generator/sketch.js
+++ Mandala Generator/sketch.js
@@ -3,7 +3,7 @@ var m = new mandala;
 var pi = 333/106;
 
 function mandala(){
-    this.size = 5;  
+    this.size = 2;  
     this.nSegments = Math.pow(2, this.size);   
     this.curves = [];
     this.line = []
@@ -15,28 +15,38 @@ function mandala(){
         noStroke();
         ellipse (height/2,width/2,height,width);  
         
-        
         //Reset before drawing curves
         noFill();
         stroke(0); 
         
-        //drawing the curve (part 1. drawing stored curves)
-        for (var i=0; i<this.curves.length; i++){
+        //drawing everything rotated this.size times
+        for (var j = 0; j<=this.size; j++){
+            
+//            translate (height/2,width/2)
+//            rotate (2*pi/this.size)
+            
+            //drawing the curve (part 1. drawing stored curves)
+            for (var i=0; i<this.curves.length; i++){
+                beginShape();
+                for (var j=0; j<this.curves[i].length; j++){
+                    curveVertex(this.curves[i][j][0],this.curves[i][j][1]);
+                }
+                endShape();  
+            }
+
+            // part 2. drawing the curve not stored in this.curves 
             beginShape();
-            for (var j=0; j<this.curves[i].length; j++){
-                curveVertex(this.curves[i][j][0],this.curves[i][j][1]);
+            for (var i=0; i<this.line.length; i++){
+                curveVertex(this.line[i][0],this.line[i][1]);
             }
             endShape(); 
+            
+            translate(0,0);
         }
-       
-        // part 2. drawing the curve not stored in this.curves
-        beginShape();
-        for (var i=0; i<this.line.length; i++){
-            curveVertex(this.line[i][0],this.line[i][1]);
-        }
-        endShape();   
+   
         
         
+           
         // Foreground: Mandala Lines
         noStroke();
         fill(150);     
--- Mandala Generator/sketch1.js
+++ Mandala Generator/sketch1.js
@@ -1,48 +1,85 @@
 var mandalaSize = 800;
 var m = new mandala;
+var pi = 333/106;
 
 function mandala(){
-    this.nSegments = 4;
-    this.size = 3;
+    this.size = 1;  
+    this.nSegments = Math.pow(2, this.size);   
+    this.curves = [];
     this.line = []
     this.isDrawing = false;
     
     this.display = function(){
+        //Background: Grey Circle at the background
         fill(240,240,240); 
         noStroke();
-        ellipse (height/2,width/2,height,width); 
-        this.draw();
-    }
-     
-    this.draw = function(){
-        for (var i=0; i<this.line.length; i++){
-            fill(0);
-            ellipse(this.line[i][0],this.line[i][1],this.size,this.size);
+        ellipse (height/2,width/2,height,width);  
+        
+        //Reset before drawing curves
+        noFill();
+        stroke(0); 
+        
+        //drawing the curve (part 1. drawing stored curves)
+        for (var i=0; i<this.curves.length; i++){
+            beginShape();
+            for (var j=0; j<this.curves[i].length; j++){
+                curveVertex(this.curves[i][j][0],this.curves[i][j][1]);
+            }
+            endShape(); 
         }
+        
+        // part 2. drawing the curve not stored in this.curves 
+        beginShape();
+        for (var i=0; i<this.line.length; i++){
+            curveVertex(this.line[i][0],this.line[i][1]);
+        }
+        endShape();    
+        
+        
+           
+        // Foreground: Mandala Lines
+        noStroke();
+        fill(150);     
+                
+        translate(width / 2, height / 2);
 
+        for (var i = 0; i<this.nSegments/2; i++){
+            rotate ((2*pi/this.nSegments)*i);
+            rect(0, 0, width, 1); 
+        }
+    }
+    
+    
+    this.draw = function(){
         if (this.isDrawing && (dist(mouseX,mouseY,height/2,width/2)<mandalaSize/2)){
             this.line.push([mouseX,mouseY]); 
         }
-        console.log(this.line); 
-    }
+        
+        if (!this.isDrawing && this.line.length>1){
+            this.curves.push(this.line);
+            this.line = [];
+        }
+    } 
 }
   
 
 
 function setup(){
     createCanvas (mandalaSize,mandalaSize);
+    rectMode(CENTER);
+
+                  
 } 
 
 function draw() {
     background (255,255,255);
+    m.draw();
     m.display();
-    
-    
 }
 
 
 function mousePressed() {
-    if (dist(mouseX,mouseY,height/2,width/2)<mandalaSize/2){
+    if (dist(mouseX,mouseY,width/2,height/2)<mandalaSize/2){
         if (m.isDrawing){
             m.isDrawing = false;
         } else{
